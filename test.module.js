dc1200f0395c624a3c40a1afc805cebf96a9a1b8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-19 18:22:38 +0800

    Updated build procedure.

--- test.module.js
+++ test.module.js
@@ -1,5 +1,5 @@
-const assert = require( "assert" );
 const asea = require( "./asea.js" );
+const assert = require( "assert" );
 
 assert.equal( asea( ).toString( ), "Symbol(server)", "should return 'Symbol(server)'" );
 
