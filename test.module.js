7b5658e6982ea6866332f152cc67a8ad7dffbb80
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 10:42:29 +0800

    Save changes.

--- test.module.js
+++ test.module.js
@@ -73,7 +73,6 @@ const path = require( "path" );
 //: @server:
 describe( "asea", ( ) => {
 
-
 	describe( "`asea( ).toString( )`", ( ) => {
 
 		it( "should be equal to 'Symbol(server)'", ( ) => {
@@ -90,7 +89,6 @@ describe( "asea", ( ) => {
 
 //: @client:
 describe( "asea", ( ) => {
-
 
 	describe( "`asea( ).toString( )`", ( ) => {
 
